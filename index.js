32b9ae7a6367a8e8812d0bb09f0e6009b40bf2b9
Andy Royle <ajroyle@gmail.com>
2016-03-21 14:09:53 +0000

    log keyspace values per database (keys, expires, avg_ttl)

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 var path = require('path');
 var util = require('util');
-var gauges = require('./gauges');
+var infoParser = require('./lib/info-parser');
+var redisClientFactory = require('./lib/redis-client-factory');
 var configDir = path.resolve(process.argv[2] || './');
 
-var redis = require('redis');
 var StatsD = require('statsd-client');
 
 util.log('starting...');
@@ -14,75 +14,37 @@ util.log(`using config from ${configDir}`);
 var statsConfig = require(path.join(configDir, 'statsd'));
 var statsdClient = new StatsD({ host: statsConfig.host, port: statsConfig.port, debug: statsConfig.debug });
 
-var buildTags = function(tags){
-  return Object.keys(tags || {})
-    .map(function(k, v){
-      return `${k}=${tags[k]}`;
-    }).join(',');
-};
-
-var parseStats = function(info, prefix, tags){
-  return info.split('\r\n').map(function(f){
-    if(!f || f.indexOf('#') > -1){
-      return;
-    }
-    var bits = f.split(':');
-
-    if(bits.length !== 2 || gauges[bits[0]] === undefined){
-      return;
-    }
-
-    var suffix = '';
-    if(tags.length > 0){
-      suffix = `,${tags}`;
-    }
-
-    var pre = '';
-    if(prefix && prefix.length > 0){
-      pre = `${prefix}.`;
-    }
-
-    return [`${pre}${bits[0]}${suffix}`, parseInt(bits[1], 10)];
-  }).filter(function(f){
-    return f !== undefined && !isNaN(f[1]);
-  });
-};
-
-var redisClients = require(path.join(configDir, 'redis')).map(function(c){
-  var cl = redis.createClient({
-    host: c.host,
-    port: c.port,
-    'enable_offline_queue': false
-  });
-
-  cl.on('error', function(err){
-    util.log(`[${c.host}] ${err}`);
-  });
-
-  cl.on('reconnecting', function(data){
-    util.log(`[${c.host}] reconnecting (delay: ${data.delay}, attempt: ${data.attempt})`);
-  });
-
-  cl.host = c.host;
-  cl.tags = buildTags(c.tags);
-  cl.prefix = c.prefix;
-
-  if(c.password){
-    cl.auth(c.password);
+var getSuffix = function(tags){
+  var suffix = '';
+  if(tags.length > 0){
+    suffix = `,${tags}`;
   }
-  return cl;
-});
+  return suffix;
+};
+
+var getPrefix = function(prefix){
+  var pre = '';
+  if(prefix && prefix.length > 0){
+    pre = `${prefix}.`;
+  }
+  return pre;
+}
+
+var redisClients = require(path.join(configDir, 'redis')).map(redisClientFactory);
 
 redisClients.forEach(function(c){
   setInterval(function(){
     c.info(function(err, res){
-      if(err){
+      if(err) {
         util.log(`[${c.host}] ${err}`);
         return;
       }
 
-      parseStats(res, c.prefix, c.tags).forEach(function(k){
-        statsdClient.gauge(k[0], k[1]);
+      var prefix = getPrefix(c.prefix);
+      var suffix = getSuffix(c.tags);
+
+      infoParser(res).forEach(function(k){
+        statsdClient.gauge(`${prefix}${k[0]}${suffix}`, k[1]);
       });
     });
   }, (statsConfig.interval || 10) * 1000);
