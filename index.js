c0e1bc6093694afbd0645958e95bde1d92d99ef8
Sebastian Pop <spop@Ve-mac-221.local>
2017-07-13 11:31:31 +0100

    Using utils to build prefix and suffix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 let defaultInterval = 10;
 let oneSecInMs = 1000;
+let zsetMetricAppendName = "zset"
 const path = require('path');
 const util = require('util');
 const infoParser = require('./lib/info-parser');
@@ -41,12 +42,15 @@ redisClients.forEach((c) => {
     });
 
     keyCounter.count(c, (err, stat) => {
+      var prefix = libUtils.getPrefix(c, zsetMetricAppendName);
+      var suffix = libUtils.getSuffix(c, zsetMetricAppendName);
+
       if (err) {
         util.log(`[${c.host}] ${err}`);
         return;
       }
 
-      statsdClient.gauge(`${c.prefix}.${stat.key}.${c.suffix}`, stat.count);
+      statsdClient.gauge(`${prefix}${stat.key}${suffix}`, stat.count);
     });
 
   }, (statsConfig.interval || defaultInterval) * oneSecInMs);
